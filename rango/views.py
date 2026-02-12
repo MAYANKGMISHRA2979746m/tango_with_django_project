838bb10a9fa566234263bd3000a8dc0da1905ab2
MAYANK MISHRA <2979746m@student.gla.ac.uk>
2026-02-12 18:57:38 +0000

    Complete Chapter 3: views, urls, templates

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,13 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    context_dict = {'boldmessage': 'This tutorial has been put together by YOU.'}
+    return render(request, 'rango/about.html', context=context_dict)
+
+
+
 
