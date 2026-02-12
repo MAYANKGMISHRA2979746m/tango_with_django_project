a078a1d442af5005e7d2fcd7e8a091f0cf1af508
MAYANK MISHRA <2979746m@student.gla.ac.uk>
2026-02-12 23:58:38 +0000

    Complete Chapter 4 and fix URL routing

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rango import views
 
 urlpatterns = [
+    path('', views.index, name='index'),          
+    path('about/', views.about, name='about'),    
+
+    path('rango/', include('rango.urls')),        
     path('admin/', admin.site.urls),
-    path('', include('rango.urls')),
 ]
+
+
 
